acf1e8797d489cc5df573ce7acda37c85c6db5cc
xtine <piroko@gmail.com>
2013-06-09 01:35:00 -0700

    clean up modal function
    - Rename to jquery.modal.js
    - Linting spacing corrections (switch to spaces)
    - Change modal init (open by calling modal function)
    - Fades to timed show/hides
    - Default top value is calculated by height of modal div
    - Prevent window scrolling while modal is open

---
+++ jquery.modal.js
@@ -0,0 +1,59 @@
+//  USAGE:
+//  $('.modal').modal();
+
+(function($){
+
+    $.fn.extend({
+
+        modal: function(options) {
+
+            var defaults = {
+                overlay: 0.8,
+                closeButton: '.close'
+            };
+
+            var overlay = $("<div id='lean_overlay'></div>");
+
+            $("body").append(overlay);
+
+            options =  $.extend(defaults, options);
+
+            var o = options, modal_id = $(this);
+
+            // $("#lean_overlay").click(function() {
+            //      close_modal(modal_id);
+            // });
+
+            $(o.closeButton).click(function() {
+                 close_modal(modal_id);
+            });
+
+            var modal_height = $(modal_id).outerHeight(),
+                modal_width = $(modal_id).outerWidth();
+
+            $('body, html').css('overflow', 'hidden');
+
+            $('#lean_overlay').fadeTo(400,o.overlay);
+
+            $(modal_id).css({
+                'position' : 'fixed',
+                'z-index': 5000,
+                'left' : 50 + '%',
+                'margin-left' : - (modal_width / 2),
+                'top' : ($(window).height() / 2) - (modal_height / 2)
+            });
+
+            $(modal_id).show(400);
+
+            e.preventDefault();
+
+            function close_modal(modal_id){
+                $("#lean_overlay").fadeOut(800);
+                $(modal_id).hide(600);
+                $('body, html').css('overflow', 'auto');
+            }
+
+        }
+    });
+
+})(jQuery);
